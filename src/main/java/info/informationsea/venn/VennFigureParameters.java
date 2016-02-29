1195c7abe717dd88b909ed383c1c8f8ef0aabe3b
OKAMURA Yasunobu <okamura@informationsea.info>
2016-02-29 14:40:53 +0900

    insert license terms into header of java files

--- src/main/java/info/informationsea/venn/VennFigureParameters.java
+++ src/main/java/info/informationsea/venn/VennFigureParameters.java
@@ -1,3 +1,21 @@
+/*
+    Venn Draw : Draw Venn Diagram
+    Copyright (C) 2016 Yasunobu OKAMURA All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package info.informationsea.venn;
 
 import lombok.Value;
@@ -8,11 +26,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * venndraw
- * Copyright (C) 2016 OKAMURA Yasunobu
- * Created on 2016/02/27.
- */
 @Value
 public class VennFigureParameters<T> {
 
--- src/test/java/info/informationsea/venn/VennDrawCLITest.java
+++ src/test/java/info/informationsea/venn/VennDrawCLITest.java
@@ -1,3 +1,21 @@
+/*
+    Venn Draw : Draw Venn Diagram
+    Copyright (C) 2016 Yasunobu OKAMURA All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package info.informationsea.venn;
 
 import org.junit.Before;
@@ -7,11 +25,6 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/**
- * VennDraw
- * Copyright (C) 2016 OKAMURA Yasunobu
- * Created on 2016/02/29.
- */
 public class VennDrawCLITest {
 
     public static final File DIST_DIR = new File(new File("build"), "test-cli");
